99a3866aa9162ef4fbd8a76b81f77f1de4afda23
Alex Hermundson <alex.hermundson@S04381-MBPR.local>
2019-12-26 20:17:04 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * @format
+ */
+
+import { AppRegistry } from "react-native";
+import App from "./App";
+
+const APP_NAME = "squaresReasonNative";
+
+AppRegistry.registerComponent(APP_NAME, () => App);
